7c1c5a99a7391c67b0a5b049db65e76683cc84f5
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2026-06-14 11:59:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-//your code here!
+const list = document.getElementById("infi-list");
 
+function addItems(count) {
+    for (let i = 0; i < count; i++) {
+        const listItem = document.createElement("li");
+        listItem.textContent = `Item ${list.children.length + 1}`;
+        list.appendChild(listItem);
+    }
+}
+
+addItems(10);
+
+function handleScroll() {
+    if (list.scrollTop + list.clientHeight >= list.scrollHeight - 1) {
+        addItems(2);
+    }
+}
+
+list.addEventListener("scroll", handleScroll);
